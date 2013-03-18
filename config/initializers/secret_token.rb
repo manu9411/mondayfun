e6d1a7c495ab23a0baafc5be39cf78b63ed6de7b
Manuel Molina <molin000@gmail.com>
2013-03-18 08:29:07 +0200

    push1

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mondayfun::Application.config.secret_token = 'abc31cba4a46306662109bf35883c87dbb285809ed9d46ac8e337f7a107ff9e677c29f72f82242b2142799eb3e881e7eeea359a294804fd139884c1ebee80292'
